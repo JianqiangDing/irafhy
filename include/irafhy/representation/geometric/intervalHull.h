d65eff8996167d0aa66fd994f5bd1fc0b1c70289
JianqiangDing <dingjianqiang0x@gmail.com>
2019-09-22 01:52:54 +0800

    refine the document

--- include/irafhy/representation/geometric/intervalHull.h
+++ include/irafhy/representation/geometric/intervalHull.h
@@ -12,43 +12,194 @@ namespace irafhy
 	class IntervalHull : public Geometry<IntervalHull>
 	{
 	private:
+		/**
+		 * @brief interval constraints of each dimension related entries
+		 */
 		std::vector<capd::interval> constraints_;
 
 	public:
+		/**
+		 * @brief static constructor of Empty interval hull specified dimensional space
+		 * @param dimension dimension of the space which the interval hull in
+		 * @return resulting Empty interval hull
+		 */
 		static IntervalHull Empty(std::size_t dimension = 0);
 
 	public:
+		/**
+		 * @brief default constructor
+		 */
 		IntervalHull();
-		IntervalHull(const IntervalHull& intervalHull)	 = default;
+		/**
+		 * @brief copy constructor with given interval hull
+		 * @param intervalHull given interval hull
+		 */
+		IntervalHull(const IntervalHull& intervalHull) = default;
+		/**
+		 * @brief move constructor with given interval hull
+		 * @param intervalHull given interval hull
+		 */
 		IntervalHull(IntervalHull&& intervalHull) noexcept = default;
+		/**
+		 * @brief constructor with given constraints
+		 * @param constraints given constraints
+		 */
 		explicit IntervalHull(const std::vector<capd::interval>& constraints);
+		/**
+		 * @brief constructor with given points
+		 * @param points given points
+		 */
 		explicit IntervalHull(const std::vector<Point>& points);
+		/**
+		 * @brief constructor with one given center and related radius which can specify the variance of each dimension
+		 * @param center point which specify the center of the interval hull
+		 * @param radius given radius
+		 */
 		IntervalHull(const Point& center, double radius);
-		IntervalHull(const capd::C0Rect2Set& set);
+		/**
+		 * @brief constructor with given state set
+		 * @param set given state set
+		 */
+		explicit IntervalHull(const capd::C0Rect2Set& set);
+		/**
+		 * @brief constructor with given coordinate of the center and related radius
+		 * @param center coordinate of the center
+		 * @param radius gvien radius
+		 */
 		IntervalHull(const Eigen::VectorXd& center, double radius);
+		/**
+		 * @brief destructor
+		 */
 		~IntervalHull() override = default;
-		std::vector<capd::interval> constraints() const;
-		bool						empty() const override;
-		int							dimension() const override;
-		std::vector<Point>			extremVertices() const;
-		Point						randInnerPoint() const;
-		Point						centroid() const;
-		bool						intersect(const IntervalHull& rhs, IntervalHull& result) const override;
-		IntervalHull				unite(const IntervalHull& rhs) const override;
-		bool						contains(const Point& point) const override;
-		bool						contains(const Eigen::VectorXd& coordinate) const override;
-		bool						contains(const IntervalHull& rhs) const;
-		capd::interval&				operator[](std::size_t index);
-		const capd::interval&		operator[](std::size_t index) const;
-		bool						operator==(const IntervalHull& rhs) const;
-		bool						operator!=(const IntervalHull& rhs) const;
-		bool						operator<(const IntervalHull& rhs) const;
-		bool						operator<=(const IntervalHull& rhs) const;
-		bool						operator>(const IntervalHull& rhs) const;
-		bool						operator>=(const IntervalHull& rhs) const;
-		IntervalHull&				operator=(const IntervalHull& rhs) = default;
-		IntervalHull&				operator=(IntervalHull&& rhs) = default;
+		/**
+		 * @brief get the constraints of the interval hull
+		 * @return the constraints of the interval hull
+		 */
+		[[nodiscard]] std::vector<capd::interval> constraints() const;
+		/**
+		 * @brief check if the interval hull is empty
+		 * @return TRUE if the interval hull's volume is zero or has empty constraints
+		 */
+		[[nodiscard]] bool empty() const override;
+		/**
+		 * @brief get the dimension of the space which the interval hull in
+		 * @return the dimension of the target space
+		 */
+		[[nodiscard]] int dimension() const override;
+		/**
+		 * @brief get the extreme vertices of the interval hull
+		 * @return the extreme vertices of the interval hull
+		 */
+		[[nodiscard]] std::vector<Point> extremeVertices() const;
+		/**
+		 * @brief randomly sample a point from the domain defined by the interval hull
+		 * @return resulting point which randomly sampled from the domain defined by the interval hull
+		 */
+		[[nodiscard]] Point randInnerPoint() const;
+		/**
+		 * @brief get the centroid of the interval hull
+		 * @return the centroid of the interal hull
+		 */
+		[[nodiscard]] Point centroid() const;
+		/**
+		 * @brief check if the current interval hull intersects with the right hand side interval hull
+		 * @param rhs right hand side interval hull
+		 * @param result intersection of two interval hulls if exists
+		 * @return TRUE if the intersection isn't empty
+		 */
+		bool intersect(const IntervalHull& rhs, IntervalHull& result) const override;
+		/**
+		 * @brief get the union of the current interval hull and right hand side interval hull
+		 * @param rhs right hand side interval hull
+		 * @return the union of two interval hulls
+		 */
+		[[nodiscard]] IntervalHull unite(const IntervalHull& rhs) const override;
+		/**
+		 * @brief check if the given point inside the domain defined by the interval hull
+		 * @param point the given point
+		 * @return TRUE if the given point inside the domain defined by the interval hull
+		 */
+		[[nodiscard]] bool contains(const Point& point) const override;
+		/**
+		 * @brief check if the given coordinate inside the domain defined by the interval hull
+		 * @param coordinate the given coordinate
+		 * @return TRUE if the given coordinate inside the domain defined by the interval hull
+		 */
+		[[nodiscard]] bool contains(const Eigen::VectorXd& coordinate) const override;
+		/**
+		 * @brief check if the given interval hull inside the current interval hull
+		 * @param rhs right hand side interval hull
+		 * @return TRUE if the given interval hull inside the current interval hull
+		 */
+		[[nodiscard]] bool contains(const IntervalHull& rhs) const;
+		/**
+		 * @brief get the constraint of specified dimension
+		 * @param index index of the dimension
+		 * @return the constraint of given dimension in interval form
+		 */
+		capd::interval& operator[](std::size_t index);
+		/**
+		 * @brief get the constraint of specified dimension
+		 * @param index index of the dimension
+		 * @return the constraint of given dimension in interval form
+		 */
+		const capd::interval& operator[](std::size_t index) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return TRUE if the two interval hulls equal to each other
+		 */
+		bool operator==(const IntervalHull& rhs) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return TRUE if the current interval hull does not equal to the given one
+		 */
+		bool operator!=(const IntervalHull& rhs) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return FALSE if the there exist at least one dimension in which the constraint of current interval hull is greater than or equal to the right hand side one's
+		 */
+		bool operator<(const IntervalHull& rhs) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return FALSE if the there exist at least one dimension in which the constraint of current interval hull is greater than the right hand side one's
+		 */
+		bool operator<=(const IntervalHull& rhs) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return FALSE if there exist at least one dimension in which the constraint of current interval hull is less than or equal to the right hand side one's
+		 */
+		bool operator>(const IntervalHull& rhs) const;
+		/**
+		 * @brief relational operator
+		 * @param rhs right hand side interval hull
+		 * @return FALSE if there exist at least one dimension in which the constraint of current interval hull less than the right hand side one's
+		 */
+		bool operator>=(const IntervalHull& rhs) const;
+		/**
+		 * @brief assignment operator
+		 * @param rhs right hand side interval hull
+		 * @return resulting interval hull
+		 */
+		IntervalHull& operator=(const IntervalHull& rhs) = default;
+		/**
+		 * @brief assignment operator
+		 * @param rhs right hand side interval hull
+		 * @return resulting interval hull
+		 */
+		IntervalHull& operator=(IntervalHull&& rhs) = default;
 	};
+
+	/**
+	 * @brief out the right hand side interval hull to standard out stream
+	 * @param out given out stream
+	 * @param rhs right hand side interval hull
+	 * @return resulting out stream
+	 */
 	std::ostream& operator<<(std::ostream& out, const IntervalHull& rhs);
 } // namespace irafhy
 #endif //REPRESENTATION_GEOMETRIC_INTERVAL_HULL_H
--- include/irafhy/representation/geometric/polytope.h
+++ include/irafhy/representation/geometric/polytope.h
@@ -14,20 +14,64 @@ namespace irafhy
 	class Polytope : public Geometry<Polytope>
 	{
 	private:
-		std::vector<Point>	 pointConstraints_;
+		/**
+		 * @brief point constraints which define the convex hull entity
+		 */
+		std::vector<Point> pointConstraints_;
+		/**
+		 * @brief half spaces constraints which define the convex hull entity
+		 */
 		std::vector<HalfSpace> halfSpaceConstraints_;
-		double				   volume_	= 0.0;
-		int					   dimension_ = 0;
+		/**
+		 * @brief volume of the convex hull
+		 */
+		double volume_ = 0.0;
+		/**
+		 * @brief dimension of the space which current convex hull in
+		 */
+		int dimension_ = 0;
 
 	private:
-		Polytope(const std::vector<Point>&	 pointConstraints,
+		/**
+		 * @brief constructor with given constraints and related information
+		 * @param pointConstraints point constraints of the convex hull entity
+		 * @param halfSpaceConstraints half spaces constraints of the convex hull entity
+		 * @param volume volume of the convex hull
+		 * @param dimension dimension of the space which the polytope in
+		 */
+		Polytope(const std::vector<Point>&	   pointConstraints,
 				 const std::vector<HalfSpace>& halfSpaceConstraints,
 				 double						   volume,
 				 int						   dimension);
-		void			   init(const convexConstraints& constraints);
+		/**
+		 * @brief initialize the polytope with given convex hull
+		 * @param constraints
+		 */
+		void init(const convexConstraints& constraints);
+		/**
+		 * @brief enumerate vertices using dual polytope method
+		 * @param halfSpaces given half spaces
+		 * @return resulting extreme vertices of the target domain defined by the given half spaces
+		 */
 		std::vector<Point> verticesEnumerationDualPly(const std::vector<HalfSpace>& halfSpaces);
+		/**
+		 * @brief enumerate vertices using intersection computation
+		 * @param halfSpaces given half spaces
+		 * @return resulting extreme vertices of the target domain defined by the given half spaces
+		 */
 		std::vector<Point> verticesEnumerationIntersect(const std::vector<HalfSpace>& halfSpaces);
+		/**
+		 * @brief enumerate extreme vertices with given constraints in interval form
+		 * @param constraints given space constraints
+		 * @return resulting extreme vertices of the target domain defined by the given constraints
+		 */
 		std::vector<Point> verticesEnumeration(const std::vector<capd::interval>& constraints);
+		/**
+		 * @brief enumerate extreme vertices with given center coordinate and related radius
+		 * @param center coordinate of the target domain
+		 * @param radius given radius
+		 * @return resulting extreme vertices of the target domain defined by the given center and related radius
+		 */
 		std::vector<Point> verticesEnumeration(const Eigen::VectorXd& center, double radius);
 
 	public:
@@ -35,7 +79,7 @@ namespace irafhy
 
 	public:
 		Polytope();
-		Polytope(const Polytope& polytope)	 = default;
+		Polytope(const Polytope& polytope)	   = default;
 		Polytope(Polytope&& polytope) noexcept = default;
 		explicit Polytope(const std::vector<Point>& points);
 		explicit Polytope(const std::vector<HalfSpace>& halfSpaces);
@@ -44,20 +88,20 @@ namespace irafhy
 		Polytope(const Point& center, double radius);
 		Polytope(const Eigen::VectorXd& center, double radius);
 		~Polytope() override = default;
-		std::vector<Point>			pointConstraints() const;
-		std::vector<HalfSpace>		halfSpaceConstraints() const;
-		Point						centroid() const;
-		std::vector<capd::interval> constraints() const;
-		std::vector<capd::interval> inscribedConstraints(const Point& centroid) const;
-		bool						empty() const override;
-		int							dimension() const override;
-		double						volume() const;
-		bool						intersect(const Polytope& rhs, Polytope& result) const override;
-		Polytope					unite(const Polytope& rhs) const override;
-		bool						contains(const Point& point) const override;
-		bool						contains(const Eigen::VectorXd& coordinate) const override;
-		Polytope&					operator=(const Polytope& rhs) = default;
-		Polytope&					operator=(Polytope&& rhs) noexcept = default;
+		[[nodiscard]] std::vector<Point>		  pointConstraints() const;
+		[[nodiscard]] std::vector<HalfSpace>	  halfSpaceConstraints() const;
+		[[nodiscard]] Point						  centroid() const;
+		[[nodiscard]] std::vector<capd::interval> constraints() const;
+		[[nodiscard]] std::vector<capd::interval> inscribedConstraints(const Point& centroid) const;
+		[[nodiscard]] bool						  empty() const override;
+		[[nodiscard]] int						  dimension() const override;
+		[[nodiscard]] double					  volume() const;
+		bool									  intersect(const Polytope& rhs, Polytope& result) const override;
+		[[nodiscard]] Polytope					  unite(const Polytope& rhs) const override;
+		[[nodiscard]] bool						  contains(const Point& point) const override;
+		[[nodiscard]] bool						  contains(const Eigen::VectorXd& coordinate) const override;
+		Polytope&								  operator=(const Polytope& rhs) = default;
+		Polytope&								  operator=(Polytope&& rhs) noexcept = default;
 	};
 	std::ostream& operator<<(std::ostream& out, const Polytope& rhs);
 } // namespace irafhy
